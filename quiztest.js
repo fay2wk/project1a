fcb5dc955fc83f692f8a76f293c6f545c3030b93
fay2wk <ngsiwai@gmail.com>
2016-06-07 22:21:45 +0800

    Update quiztest.js

--- quiztest.js
+++ quiztest.js
@@ -1,97 +1,86 @@
-/* global */
+/* global $ */
 /* exports isGameOver, whoWon, playTurn, restart, currentQuestion, correctAnswer, numberOfAnswers */
-
 var questions = [{
   question: 'How long is an eon?',
   choices: ['A: 1 million years.', 'B: 1 thousand years. ', 'C: 1 billion years.', 'D: 1 hundred years.'],
-  correctAnswer: 'C'
+  correctAns: 2
 }, {
   question: "What was Charlie Brown's father's job?",
   choices: ['A: Barber.', 'B: Plumber. ', 'C: Accountant.', 'D: Mechanic'],
-  correctAnswer: 'A'
+  correctAns: 0
 }, {
-  question: 'When did the Beatles win their first Grammy?',
-  choices: ['A: 1966.', 'B: 1967. ', 'C: 1965.', 'D: 1964'],
-  correctAnswer: 'C'
+  question: 'How long is an eon?',
+  choices: ['A: 1 million years.', 'B: 1 thousand years. ', 'C: 1 billion years.', 'D: 1 hundred years.'],
+  correctAns: 2
 }, {
-  question: 'What color was the first Model T Ford?',
-  choices: ['A: Blue.', 'B: Black. ', 'C: Silver.', 'D: Gray'],
-  correctAnswer: 'B'
+  question: "What was Charlie Brown's father's job?",
+  choices: ['A: Barber.', 'B: Plumber. ', 'C: Accountant.', 'D: Mechanic'],
+  correctAns: 0
 }]
 
-var currentQuestion = 0
-var player = 1
-function playTurn () {
-  if (isGameOver() === true) {
-    return false
-  } else if (player === 1) {
-    player = 2 }
-      else player = 1
-  }
+var currentQn = 0
+var player1Score = 0
+var player2Score = 0
+var gameOver = false
 
-function numberOfQuestions (num) {
-    return (questions.length)
-  }
-
-function currentQuestion () {
-    return currentQuestion
-  }
-
-function correct (answer) {
-    if (answer === questions.correctAnswer) {
-      return true
-    } else {
-      return false
-  }
+function numberOfQuestions () {
+  return questions.length
 }
 
-var player1Score = 0
-function getPlayer1Score () {
-    if ((currentQuestion % 2 === 1) && correct === true) {
-      player1Score ++
-    } else {
-      return
-    }
-  }
-
-var player2Score = 0
-function getPlayer2Score () {
-  if ((currentQuestion % 2 === 0) && correct === true) {
-   player2Score++
-  } else {
-    return
-  }
+function currentQuestion () {
+  console.log('current' + currentQn)
+  return currentQn
 }
 
 function numberOfAnswers () {
-  return questions.choices.length
+  return questions[currentQn].choices.length
 }
 
-function whoWon () {
-  if (player1Score > player2Score) {
-    return 1
-  } else if (player2Score > player1Score) {
-    return 2
-  } else if (player1Score === player2Score) {
-    return 3
-  }
-  return 0
+function correctAnswer () {
+  console.log('correct answer '+ questions[currentQn].correctAns)
+  return questions[currentQn].correctAns
 }
 
-function isGameOver () {
-  if (whoWon() === 0) {
+function playTurn (choice) {
+  if (gameOver === true) {
     return false
-  } else if (whoWon()) {
-    return true
   }
+  var correct = false
+  if (choice === questions[currentQn].correctAns) {
+  correct = true
+    if ((currentQn % 2) === 1) {
+      player1Score++
+    } else {
+      player2Score++
+    }
+  currentQn++
+  }
+  if (currentQuestion === numberOfQuestions()) {
+    gameOver = true
+  }
+  return correct
 }
 
 function restart () {
-  if (isGameOver() === true) {
-    currentQuestion = 0
-    player1Score = 0
-    player2Score = 0
-  } else {
-    return false
-  }
+   if (gameOver) {
+     currentQn = 0
+     player1Score = 0
+     player2Score = 0
+   }
+ }
+
+function isGameOver () {
+  return gameOver
+}
+
+function whoWon () {
+  if (gameOver === false) {
+    return 0 }
+    else if (player1Score > player2Score) {
+      return 1
+    } else if (player1Score < player2Score) {
+      return 2
+    } else if (player1Score === player2Score) {
+      return 3
+    }
 }
